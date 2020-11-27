4e407184c930b7cf915a178cc2c78c8e26c22a6b
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-27 13:49:01 +0100

    Definidas Variables Ejercicio4

---
+++ Ejercicio4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ejercicio4{
+	
+	public static void main (String args[]){
+		
+		int n;
+		
+		Scanner a = new Scanner(System.in);
+		
+		System.out.println("Introduzca un número del 1 al 7");
+		n=a.nextInt();
+		
+		
+		
+		
+	}
+}
+
+		
+		
