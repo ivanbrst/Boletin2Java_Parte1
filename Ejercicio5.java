d474931b9c0eb7a77471007b6871acd39ffabcff
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-27 14:14:41 +0100

    Terminado Ejercicio5

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -8,13 +8,21 @@ public class Ejercicio5{
 		int ano;
 		int bisiesto;
 		
+		bisiesto=0;
+		
 		Scanner a=new Scanner(System.in);
 		
 		System.out.println("Introduzca o ano");
 		ano=a.nextInt();
 		
+		if((ano%4==0) && (ano%100!=0) | (ano%400==0))
+			{bisiesto=1;}
 		
+		if(bisiesto==1)
+		{System.out.println("O ano introducido é bisiesto");}
+		else{System.out.println("O ano introducido non é bisiesto");}
 		
+
 		
 		
 		
