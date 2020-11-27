7ec1d8d1df09860af99a1e1adf6ade5905ee27e6
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-27 14:08:14 +0100

    Definidas Variables Ejercicio5

---
+++ Ejercicio5.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ejercicio5{
+	
+	public static void main (String args[]){
+		
+		
+		int ano;
+		int bisiesto;
+		
+		Scanner a=new Scanner(System.in);
+		
+		System.out.println("Introduzca o ano");
+		ano=a.nextInt();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
