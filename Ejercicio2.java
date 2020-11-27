08474627f99e8edf12ae8ff617a561a24c92641f
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-27 13:08:55 +0100

    Planteadas Variables Ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio2 {
+	
+	public static void main (String args[]{
+		
+		double valor;
+		double precio;
+		
+		Scanner a=new Scanner(System.in);
+		
+		System.out.println("Introduzca o valor do producto con punto para os céntimos");
+		valor=a.nextInt();
+		
+		
+		
+		
+		
+		
+		
+	}
+}
