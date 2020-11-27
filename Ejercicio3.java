7cc2ad1b82897b3da475e93ae6c2938f4e31a159
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-27 13:38:32 +0100

    Definidas Variables Ejercicio3

---
+++ Ejercicio3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Ejercicio3{
+	
+	public static void main (String args[]){
+		
+		int dni;
+		int r;
+		
+		Scanner a=new Scanner(System.in);
+		
+		System.out.println("Introduzca o seu DNI sen letra");
+		dni=a.nextInt();
+		
+	
+		
+
+	
+	}
+}
