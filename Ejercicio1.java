7c6564c042b8b8166a30fcb0b5a691027997daee
Ivan Gil Conde <ivanbrst2@gmail.com>
2020-11-27 12:59:24 +0100

    Terminado Ejercicio1

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,18 +1,25 @@
-import java.util.Scanner
+import java.util.Scanner;
 
 public class Ejercicio1 {
 	
 	public static void main (String args[]) {
 		
-		double n;
-		double m;
+		int n;
+		int m;
+		int r;
 		
+		Scanner a=new Scanner(System.in);
 		
+		System.out.println("Introduzca o primeiro número");
+		n=a.nextInt();
 		
+		System.out.println("Introduzca o segundo número");
+		m=a.nextInt();
 		
-		
-		
-		
-		
+		if((n%2==0)&&(m%2==0)&&(n<50)&&(m<=500)&&(m>=100))
+			{r=n+m;
+			System.out.println("El resultado de su suma es "+r+"");}
+		else{System.out.println("Error");}
+				
 	}
 }
